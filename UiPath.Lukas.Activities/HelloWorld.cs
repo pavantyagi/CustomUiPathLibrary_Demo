c195144bea13b53632953b3d3df9a2fdc92693b5
Lukas Larsed <git@lukaslarsed.com>
2016-10-14 09:23:46 +0200

    First commit

---
+++ UiPath.Lukas.Activities/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+
+namespace UiPath.Lukas.Activities
+{
+    public class HelloWorld : CodeActivity
+    {
+        [Category("Output")]
+        public OutArgument<string> Hello { get; set; }
+
+        public string GetHello() { return "Hello from GetHello()"; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            Hello.Set(context, "Hello from Property!");
+        }
+    }
+}
